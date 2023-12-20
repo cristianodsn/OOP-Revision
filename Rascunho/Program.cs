93d1271494c91c33d9b00bb0ca5e9d8f7607870c
Cristiano Castiel <cristianoct03@gmail.com>
2023-12-20 12:55:55 -0300

    OOP-Exercise01

---
+++ Rascunho/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rascunho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle;
+            Console.WriteLine("Enter the width and height of the rectangle:");
+            double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double heigth = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            rectangle = new Rectangle();
+            rectangle.Width = width;
+            rectangle.Height = heigth;
+
+            Console.WriteLine("Area: " + rectangle.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimeter: " + rectangle.Peremeter().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: " + rectangle.Diagonal.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rascunho/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rascunho
+{
+    class Rectangle
+    {
+        public double Width;
+        public double Height;
+
+        public double Area()
+        {
+            return Width * Height;
+        }
+
+        public double Peremeter()
+        {
+            return 2*(Width + Height);
+        }
+
+        public double Diagonal => Math.Sqrt(Math.Pow(Width, 2) + Math.Pow(Height, 2));
+        
+    }
+}
